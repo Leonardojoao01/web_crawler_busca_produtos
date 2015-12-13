05fb3afeb86e7c95c8099f37134bf78d618f07e8
Leonardojoao01 <ldsjoao@inf.ufpel.edu.br>
2015-12-13 09:12:05 -0200

    OG

--- pag.py
+++
@@ -1,6 +0,0 @@
-import urllib
-
-f = urllib.urlopen("http://codare.net")
-contents = f.read()
-f.close()
-print contents
--- regex/ponto_frio_search.py
+++ regex/ponto_frio_search.py
@@ -3,22 +3,49 @@ import requests
 import sys 
 import re
 
+## get_link >> get_name & get_cash
+
 class product(object):
     name = ""
     cash = ""
-    
+    link21 = []
+
     def set_name(self, url):
-	requestResult = requests.get(url)
+		requestResult = requests.get(url)
     	matches = re.findall('<b itemprop="name">[^<]*', requestResult.text)
     	self.name = matches[0].split(">")[1]
 
     def get_name(self):
-	return self.name
+		return self.name
 
     def set_cash(self, url):
-	requestResult = requests.get(url)
-	matches = re.findall('<i class="sale price">[^<]*', requestResult.text)
-	self.cash = matches[0].split(">")[1]
+		requestResult = requests.get(url)
+		matches = re.findall('<i class="sale price">[^<]*', requestResult.text)
+		self.cash = matches[0].split(">")[1]
 	
     def get_cash(self):
-	return self.cash
+		return self.cash
+
+	def set_link(self, url):	# Pega o link de cada produto da pag
+		#lista = []
+    	i = 0
+    	r = requests.get(url)
+    	soup = BeautifulSoup(r.text)
+    	
+    	for a in soup.findAll('a',{'class':'link url'}):
+    	    link = a['title']
+    	    
+    	    lista.append(link)
+    	    links = str(link).strip('[]')
+			self.link21[i] = links
+			i += 1
+
+		return False
+
+	def get_link(self, pos):
+		return self.link21[pos]
+
+	#def set_DB(self, name, cash, link):
+		#Conecta DB
+
+
