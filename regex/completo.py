a86fc8701ba4a9381c771ee7cce9f3fc78b53f02
Leonardojoao01 <ldrsjoao@inf.ufpel.edu.br>
2017-11-18 19:26:35 -0200

    Corrigido o erro da busca de produtos em uma página

--- regex/completo.py
+++ regex/completo.py
@@ -5,17 +5,17 @@ import urllib2
 from multiprocessing.dummy import Pool as ThreadPool
 from threading import Thread
 
-val = 0
+val = 1
 pagina = True
 
 
 while pagina == True:
-	link = ("http://search.pontofrio.com.br/search?p=Q&lbc=pontofrio-br&uid=383564926&ts=custom&w=jogos%20de%20xbox%20360&af=&isort=score&method=and&view=grid&srt="+str(val))
+	link = ("https://search.pontofrio.com.br/?strBusca=jogos%20xbox%20360&paginaAtual="+str(val))
 
 	#html = requests.get(link) #Baixa o html da pagina
 
 	#variavel = str(html.content[:])
 
 	pagina = buscar_links(link)
-	val = val + 20
+	val = val + 1
 	print val
--- regex/ponto_frio_links.py
+++ regex/ponto_frio_links.py
@@ -11,19 +11,22 @@ def buscar_links(url):
     lista = []
     i = 0
     r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")     # N lembro, tem haver com a biblioteca de PARSE
-    #print soup
+    soup = BeautifulSoup(r.text, "lxml")     # N lembro, tem haver com a biblioteca de PARSE
+    #print soup.findAll('a',{'class':'link url'})
     for a in soup.findAll('a',{'class':'link url'}):#href=True):    # Procura dentro o códido HTML essa conf, para depois encontrar o link
         #link = a['title']#a['href']
         link = a['href']        # HREF é a tag onde encontra-se o link para acessar o jogo
+        #print link
         i += 1
         lista.append(link)      # Adicina os links do produtos em uma pilha/fila para posteriormente acessar os produtos
-        links = str(link).strip('[]')#.replace("u", "")#.replace("'", "")
-        
-	buscar_links2(links)          # Chama o método para acessar o link de cada produto, pegando o nome e valor 
-	print links
+        #links = str(link).strip('[]')#.replace("u", "")#.replace("'", "")
+    
+    for link_produto in lista:             # Percorre a lista em busca dos links
+
+      buscar_links2(link_produto)          # Chama o método para acessar o produto, pegando o nome e valor 
+    #print lista
   #  if i != 0:
-	return True
+    return True
    # print "\n>> Total = %d \n" % i
    # sys.exit() 
    # return False
